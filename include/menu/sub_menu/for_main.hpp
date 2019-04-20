d06c558cd4214e04fd4b905082159fa1dba9b527
jeongsik-lee <a2819z2@gmail.com>
2019-04-21 00:34:14 +0900

    [Delete] delete menu interface

--- include/menu/sub_menu/for_main.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef MENU_ITEMS
-#define MENU_ITEMS
-
-#include "./register_menu.hpp"
-#include "./login_menu.hpp"
-
-#endif /* ifndef MENU_ITEMS */
--- include/menu/sub_menu/register_menu.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef REGISTER_MENU_HPP
-#define REGISTER_MENU_HPP 
-
-#include "menu_item.hpp"
-
-class Register_menu : public Menu_item
-{};
-
-#endif /* ifndef REGISTER_MENU_HPP */
